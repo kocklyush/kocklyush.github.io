8cf533f3faef9fea456423dd31cecf1fd2a0735f
kocklyush <125636063+kocklyush@users.noreply.github.com>
2025-04-05 03:45:54 +0300

    Add files via upload

---
+++ form-add-task-component.js
@@ -0,0 +1,36 @@
+import {createElement} from '../framework/render.js'; 
+
+
+function createFormAddTaskComponentTemplate() {
+    return (
+        `<form class="addnewtaskform">
+            <label class="addnewtaskform-label">Новая задача</label><br>
+            <div class="new-item-element">
+                <input class="new-item-input" placeholder="Название задачи...">
+                <button class="new-item-add-button">+ Добавить</button>
+            </div>
+        </form>`
+      );
+}
+
+
+export default class FormAddTaskComponent {
+  getTemplate() {
+    return createFormAddTaskComponentTemplate();
+  }
+
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+
+    return this.element;
+  }
+
+
+  removeElement() {
+    this.element = null;
+  }
+}
